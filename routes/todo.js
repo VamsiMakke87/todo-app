8574347b33a143f2e979893ff6a5a2e389a6e9a4
vamsimakke@gmail.com <vamsimakke@gmail.com>
2025-03-26 10:25:52 -0400

    Added GET TODO route

--- routes/todo.js
+++ routes/todo.js
@@ -2,11 +2,24 @@ const router = require("express").Router();
 const Todo = require("../models/todo");
 const User = require("../models/user");
 
-router.get("/", (req, res) => {
-  try{
+router.get("/", async (req, res) => {
+  try {
+    const user = await User.findById(req.body.userId);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "User Not Found!!", isSuccessMessage: false });
+    }
 
-  }catch(err){
+    const todos = await Todo.find({ userId: user._id });
+
+    res.status(200).json(todos);
+  } catch (err) {
     console.log(err);
+    res.status(500).json({
+      message: "Internal Server Error! Please try again later!",
+      isSuccessMessage: false,
+    });
   }
 });
 
@@ -28,15 +41,13 @@ router.post("/", async (req, res) => {
 
     const newTodo = new Todo(todoData);
     await newTodo.save();
-    res.status(200).json({"message":"Todo Created Succesfully!"});
+    res.status(200).json({ message: "Todo Created Succesfully!" });
   } catch (err) {
     console.log(err);
-    res
-      .status(500)
-      .json({
-        message: "Internal Server Error! Please try again later!",
-        isSuccessMessage: false,
-      });
+    res.status(500).json({
+      message: "Internal Server Error! Please try again later!",
+      isSuccessMessage: false,
+    });
   }
 });
 
