275c62b31c5c8ec902c6cf07c57a217c8ec8e71e
vamsimakke@gmail.com <vamsimakke@gmail.com>
2025-03-26 10:49:13 -0400

    Added Update TODO route

--- routes/todo.js
+++ routes/todo.js
@@ -51,6 +51,40 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) {
+      return res
+        .status(404)
+        .json({ message: "TODO not found", isSuccessMessage: false });
+    }
+
+    if (req.body.userId === todo.userId) {
+      const updatedTODO = await Todo.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+
+      console.log(updatedTODO);
+
+      res
+        .status(200)
+        .json({ message: "TODO Updated Succesfully", isSuccessMessage: true });
+    } else {
+      res.status(403).json({
+        message: "Operation not allowed! Cannot delete others TODO",
+        isSuccessMessage: false,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Internal Server Error! Please try again later!",
+      isSuccessMessage: false,
+    });
+  }
+});
+
 router.delete("/:id", async (req, res) => {
   try {
     const todo = await Todo.findById(req.params.id);
