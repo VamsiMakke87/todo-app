3f82cd7d77b49d291ca45764638dd373159f5573
vamsimakke@gmail.com <vamsimakke@gmail.com>
2025-03-28 11:37:02 -0400

    Added login form validations and integrated with backend

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,12 +1,54 @@
-import React, { useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FaRegEye } from "react-icons/fa6";
 import { FaRegEyeSlash } from "react-icons/fa6";
+import AppContext from "../AppContext";
 
 const Login = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
   const [togglePassword, setTogglePassword] = useState("password");
+  const navigate = useNavigate();
+  const { postReq, setSuccessMsg, setErrorMsg } = useContext(AppContext);
+
+    useEffect(()=>{
+        const token = localStorage.getItem("token");
+        if(token){
+            navigate("/");
+        }
+    },[]);
+
+  const validateForm = async () => {
+    try {
+      const email = emailRef.current.value;
+      const password = passwordRef.current.value;
+
+      if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        setErrorMsg("Please enter a valid email");
+        emailRef.current.focus();
+        return;
+      } else {
+        const res = await postReq("/api/login", {
+          email: email,
+          password: password,
+        });
+
+        const jsonData = await res.json();
+        if (res.ok) {
+          setSuccessMsg(jsonData.message);
+          localStorage.setItem("token", jsonData.token);
+          localStorage.setItem("userId",jsonData.userId);
+          console.log(jsonData);
+        //   navigate("/login");
+        } else {
+          setErrorMsg(jsonData.message);
+        }
+      }
+    } catch (err) {
+      setErrorMsg("Unknown error occured! Please try again!");
+    }
+  };
+
   return (
     <div className="justify-center items-center h-screen w-screen flex text-center w-6/12">
       <div className="bg-white md:w-5/12 sm:w-8/12 w-10/12   p-6 px-10 rounded-lg shadow-lg">
@@ -43,7 +85,10 @@ const Login = () => {
               </div>
             </div>
           </div>
-          <div className="w-full bg-black text-white p-2 rounded-lg hover:bg-white hover:text-black border border-black cursor-pointer">
+          <div
+            onClick={validateForm}
+            className="w-full bg-black text-white p-2 rounded-lg hover:bg-white hover:text-black border border-black cursor-pointer"
+          >
             Login
           </div>
           <div>
